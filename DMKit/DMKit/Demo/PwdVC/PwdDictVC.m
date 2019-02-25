a47985fccab9d2cb8d1f49eb7b072570777e02ee
daimu <258141764@qq.com>
2019-02-25 18:04:46 +0800

    Update PwdDictVC.m

--- DMKit/DMKit/Demo/PwdVC/PwdDictVC.m
+++ DMKit/DMKit/Demo/PwdVC/PwdDictVC.m
@@ -378,17 +378,18 @@
 
 - (void)chaifen
 {
-    NSString *cutPath = @"/Users/daimu/Desktop/pwd/cut.txt";
+    NSString *cutPath = @"/Users/imac-03/Desktop/Ljl/WordList/cut.txt";
     NSArray *paths = @[
-                       @"/Users/daimu/Desktop/pwd/1.txt",
-                       @"/Users/daimu/Desktop/pwd/2.txt",
-                       @"/Users/daimu/Desktop/pwd/3.txt"
+                       @"/Users/imac-03/Desktop/Ljl/WordList/弱口令合集/1.txt",
+                       @"/Users/imac-03/Desktop/Ljl/WordList/弱口令合集/2.txt",
+                       @"/Users/imac-03/Desktop/Ljl/WordList/弱口令合集/3.txt",
+                       @"/Users/imac-03/Desktop/Ljl/WordList/弱口令合集/4.txt"
                        ];
-    NSString *tmpDirPath = @"/Users/daimu/Desktop/pwd/tmp";
+    NSString *tmpDirPath = @"/Users/imac-03/Desktop/Ljl/WordList/tmp";
     
-    NSString *allPath = @"/Users/daimu/Desktop/pwd/all.txt";
+    NSString *allPath = @"/Users/imac-03/Desktop/Ljl/WordList/all1.txt";
     
-    
+    NSString *cutResultDirPath = @"/Users/imac-03/Desktop/Ljl/WordList/cutResult";
     
     // 1. 生成分割词数组
     NSString *cutStrs = [NSString stringWithContentsOfFile:cutPath encoding:NSUTF8StringEncoding error:nil];
@@ -400,15 +401,29 @@
     // 2. 拼接成最大的数组
     NSMutableArray *muarray = [NSMutableArray array];
 //    for (NSString * path in paths) {
-//        NSString *str = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
-//        NSLog(@"create str");
-//        str = [str stringByReplacingOccurrencesOfString:@"\r" withString:@""];
-//        NSLog(@"replace str");
-//        NSArray *array = [str componentsSeparatedByString:@"\n"];
-//        NSLog(@"create array");
-//        [muarray addObjectsFromArray:array];
-//        NSLog(@"merge array");
+//        @autoreleasepool {
+//            NSString *str = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+//            NSLog(@"create str");
+//            str = [str stringByReplacingOccurrencesOfString:@"\r" withString:@""];
+//            NSLog(@"replace str");
+//            str = [str stringByAppendingString:@"\n"];
+//            NSLog(@"str + \n");
+//            NSFileHandle *handel = [NSFileHandle fileHandleForUpdatingAtPath:allPath];
+//            [handel seekToEndOfFile];
+//            NSLog(@"handel allready");
+//            [handel writeData:[str dataUsingEncoding:NSUTF8StringEncoding]];
+//            NSLog(@"write to file");
+//            [handel closeFile];
+//        }
 //    }
+    
+    
+    //[self cutWithIndex:0 cutArray:cutArr inFile:allPath tmpDirPath:tmpDirPath];
+    
+    [self cutWithIndex:50 cutArray:cutArr inFile:@"/Users/imac-03/Desktop/Ljl/WordList/tmp/49big_end.txt" tmpDirPath:tmpDirPath resultDirPath:cutResultDirPath];
+    
+    
+    
     @autoreleasepool {
         NSString *str = [NSString stringWithContentsOfFile:allPath encoding:NSUTF8StringEncoding error:nil];
         NSLog(@"create str over");
@@ -448,6 +463,160 @@
     }
     
 }
+
+- (void)cutWithIndex:(int)index
+            cutArray:(NSArray *)cutArray
+              inFile:(NSString *)inFile
+          tmpDirPath:(NSString *)tmpDirPath
+       resultDirPath:(NSString *)resultDirPath
+{
+    if (index >= cutArray.count) {
+        NSLog(@"chai fen end");
+        return;
+    }
+    
+    NSString *cutStr = cutArray[index];
+    
+    NSString *pathSmall = [NSString stringWithFormat:@"%@/%d.txt",resultDirPath,index];
+    NSString *pathBig = [NSString stringWithFormat:@"%@/%dbig.txt",tmpDirPath,index];
+    
+    BOOL result = [@"" writeToFile:pathSmall atomically:YES encoding:NSUTF8StringEncoding error:NULL];
+    NSLog(@"%@ create : %d",pathSmall,result);
+    
+    
+    result = [@"" writeToFile:pathBig atomically:YES encoding:NSUTF8StringEncoding error:NULL];
+    NSLog(@"%@ create : %d",pathBig,result);
+    
+    
+    NSFileHandle *handel = [NSFileHandle fileHandleForReadingAtPath:inFile];
+    [handel seekToEndOfFile];
+    unsigned long long size = handel.offsetInFile;
+    unsigned long long lastSize = 0;
+    _allCount = size;
+    _start = [NSDate date];
+    _oldUsedTime = 0;
+    int i = 0;
+    while (size - lastSize > 0 ) {
+        @autoreleasepool {
+            _currentIndex = lastSize;
+            [handel seekToFileOffset:lastSize];
+
+            NSData *data = nil;
+            NSString *str = nil;
+            unsigned long long readSize = 50 * 1024 * 1024 + 1;
+            while (str == nil && --readSize > 0) {
+                NSLog(@"%llu",readSize);
+                data = [handel readDataOfLength:readSize];
+                str  = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            }
+            if (str == nil || str.length < 1) {
+                break;
+            }
+            NSRange range = [str rangeOfString:@"\n" options:NSBackwardsSearch];
+            str = [str substringToIndex:range.location];
+            NSData *tmpData = [str dataUsingEncoding:NSUTF8StringEncoding];
+            readSize = tmpData.length;
+            
+            NSArray *array = [str componentsSeparatedByString:@"\n"];
+            if (array.count == 0 || readSize == 0) {
+                break;
+            }
+            
+            NSMutableArray *smallArr = [NSMutableArray array];
+            NSMutableArray *bigArr = [NSMutableArray array];
+            for (int j = 0; j<array.count; j++) {
+                _runOverCount++;
+                NSString *tmpStr = array[j];
+                if ([tmpStr compare:cutStr] == NSOrderedAscending) {
+                    [smallArr addObject:tmpStr];
+                } else {
+                    [bigArr addObject:tmpStr];
+                }
+            }
+            
+            
+            NSString *smallStr = [smallArr componentsJoinedByString:@"\n"];
+            smallStr = [smallStr stringByAppendingString:@"\n"];
+            NSFileHandle *smallHandle = [NSFileHandle fileHandleForUpdatingAtPath:pathSmall];
+            [smallHandle seekToEndOfFile];
+            [smallHandle writeData:[smallStr dataUsingEncoding:NSUTF8StringEncoding]];
+            [smallHandle closeFile];
+            NSLog(@"small array write to file");
+            
+            NSString *bigStr = [bigArr componentsJoinedByString:@"\n"];
+            bigStr = [bigStr stringByAppendingString:@"\n"];
+            NSFileHandle *bigHandle = [NSFileHandle fileHandleForUpdatingAtPath:pathBig];
+            [bigHandle seekToEndOfFile];
+            [bigHandle writeData:[bigStr dataUsingEncoding:NSUTF8StringEncoding]];
+            [bigHandle closeFile];
+            NSLog(@"big array write to file");
+            
+            i++;
+            lastSize += readSize;
+            NSLog(@"%d---%llu",i,readSize);
+        }
+    }
+
+    [handel closeFile];
+    index++;
+    [self cutWithIndex:index cutArray:cutArray inFile:pathBig tmpDirPath:tmpDirPath resultDirPath:resultDirPath];
+}
+
+
+
+
+
+//
+//- (void)bigFilePinjie
+//{
+//    NSString *path4 = @"/Users/imac-03/Desktop/Ljl/WordList/弱口令合集/4.txt";
+//
+//    NSFileHandle *handel4 = [NSFileHandle fileHandleForReadingAtPath:path4];
+//    [handel4 seekToEndOfFile];
+//    unsigned long long size = handel4.offsetInFile;
+//    unsigned long long lastSize = 0;
+//    _allCount = size;
+//    _start = [NSDate date];
+//    int i = 0;
+//    while (size - lastSize < 10) {
+//        @autoreleasepool {
+//            _currentIndex = lastSize;
+//            [handel4 seekToFileOffset:lastSize];
+//
+//            NSData *data = nil;
+//            NSString *str = nil;
+//            unsigned long long readSize = 500 * 1024 * 1024 + 1;
+//            while (str == nil) {
+//                readSize--;
+//                NSLog(@"%llu",readSize);
+//                data = [handel4 readDataOfLength:readSize];
+//                str  = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+//            }
+//            NSRange range = [str rangeOfString:@"\n" options:NSBackwardsSearch];
+//            str = [str substringToIndex:range.location];
+//            NSData *tmpData = [str dataUsingEncoding:NSUTF8StringEncoding];
+//            readSize = tmpData.length;
+//            str = [str stringByReplacingOccurrencesOfString:@"\r" withString:@""];
+//            NSLog(@"replace str");
+//            NSFileHandle *handel = [NSFileHandle fileHandleForUpdatingAtPath:allPath];
+//            [handel seekToEndOfFile];
+//            NSLog(@"handel allready");
+//            [handel writeData:[str dataUsingEncoding:NSUTF8StringEncoding]];
+//            NSLog(@"write to file");
+//            [handel closeFile];
+//
+//            NSString *outpath = [NSString stringWithFormat:@"%@/%d.txt",tmpDirPath,i];
+//            [str writeToFile:outpath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+//            i++;
+//            lastSize += readSize;
+//            NSLog(@"%d---%llu",i,readSize);
+//        }
+//    }
+//
+//}
+
+
+
 
 - (int)indexOfArr:(NSArray *)arr str:(NSString *)str
 {
